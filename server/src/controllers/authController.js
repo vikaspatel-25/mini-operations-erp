c691fdf39023415d15b1792f7d7683401ae84ce1
cricrankers <cricrankers@gmail.com>
2026-09-05 19:24:52 +0530

    same site issue solved

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -24,7 +24,7 @@ export async function login(req, res) {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "lax",
+      sameSite: "none",
       maxAge: 24 * 60 * 60 * 1000,
     });
 
@@ -51,7 +51,7 @@ export function logout(req, res) {
   res.clearCookie("token", {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "lax",
+    sameSite: "None",
   });
 
   return res.status(200).json({
