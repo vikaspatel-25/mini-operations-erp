e63eaec0cdfe88d7ae8175bbd47898751cc77bc9
cricrankers <cricrankers@gmail.com>
2026-09-05 11:44:05 +0530

    f-orderRoutes.js

--- server/src/routes/orderRoutes.js
+++ server/src/routes/orderRoutes.js
@@ -1,8 +1,8 @@
 import express from "express";
 
 import {
-    getOrdersRequest,
-    createOrder
+  getOrdersRequest,
+  createOrder,
 } from "../controllers/orderController.js";
 
 import { requireAuth } from "../middleware/authMiddleware.js";
@@ -10,17 +10,8 @@ import { requireRole } from "../middleware/roleMiddleware.js";
 
 const router = express.Router();
 
-router.get(
-    "/",
-    requireAuth,
-    getOrdersRequest
-);
+router.get("/", requireAuth, getOrdersRequest);
 
-router.post(
-    "/",
-    requireAuth,
-    requireRole("ADMIN", "SALES"),
-    createOrder
-);
+router.post("/", requireAuth, requireRole("ADMIN", "SALES"), createOrder);
 
 export default router;
