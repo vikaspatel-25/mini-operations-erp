5649349d39a06bd7b40f183fede9fc4ca67675bf
cricrankers <cricrankers@gmail.com>
2026-09-05 17:23:48 +0530

    client deployment fixes

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
     plugins: [react()],
     resolve: {
         alias: {
-            "@": path.resolve(process.cwd(), "src")
-        }
+      "@": path.resolve(__dirname, "./src"),
+    },
     }
 });
